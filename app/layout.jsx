f961fbe468993fc6e5573de52b5e97cba7e7d925
Ian Will <github@iancw.io>
2026-02-16 07:10:26 -0500

    Remove white text

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,7 +4,7 @@ import { Header } from '../components/header';
 
 export const metadata = {
     title: {
-        template: '%s | Netlify',
+        template: '%s | OM Recipes',
         default: 'OM Recipes'
     }
 };
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
             <head>
                 <link rel="icon" href="/favicon.svg" sizes="any" />
             </head>
-            <body className="antialiased text-white">
+            <body className="antialiased">
                 <div className="flex flex-col min-h-screen px-6 sm:px-12">
                     <div className="flex flex-col w-full max-w-5xl mx-auto grow">
                         <Header />
